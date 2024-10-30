1850c0bf390549ef0a64814310fb537152032c77
Michael <micgaelma@gmail.com>
2024-10-30 00:26:30 -0500

    Agregado algunas modificaciones

--- src/main/java/com/appWeb/ClinicaDental/controlador/HorarioController.java
+++ src/main/java/com/appWeb/ClinicaDental/controlador/HorarioController.java
@@ -1,5 +1,7 @@
 package com.appWeb.ClinicaDental.controlador;
 
+import com.appWeb.ClinicaDental.Recursos.Sesion;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/horario")
 public class HorarioController {
+    @Autowired
+    private Sesion sesion;
 
     @GetMapping
     public String horario() {
+
         return "Horario";
     }
 }
